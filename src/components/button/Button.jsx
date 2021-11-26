89f45d14fa8172db12ae01b314c48c97c88dc5ae
Zorica Janjic <zorica.janjic@uk.kps.com>
2021-11-26 10:50:20 +0100

    Further styling menu

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,11 +3,12 @@ import UniversalLink from "../universalLink/UniversalLink"
 import Arrow from "../../assets/btnArrow.svg"
 
 const Button = props => {
+  const { icon = true, className, link, children } = props
   return (
-    <UniversalLink className={`button ${props.className}`} to={props.link}>
+    <UniversalLink className={`button ${className}`} to={link}>
       <div className="button__content-wrapper">
-        {props.children}
-        <Arrow className="button__arrow" />
+        {children}
+        {icon ? <Arrow className="button__arrow" /> : ""}
       </div>
     </UniversalLink>
   )
