abbf26a31fa5ed3b768d491a705e5c0c2f2d7e37
armin busatlic <dzarma.busatlic@gmail.com>
2021-11-26 12:46:12 +0100

    Replacing png with SVG in Star component

--- src/components/testimonialsPageSection/Stars.jsx
+++ src/components/testimonialsPageSection/Stars.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import Star from "../../assets/star.svg"
 
 const Stars = props => {
   let data = props.starsData
@@ -8,7 +8,7 @@ const Stars = props => {
   for (let i = 0; i < data; i++) {
     stars.push(
       <span className="star-wrapper" key={Math.random()}>
-        <StaticImage src="../../images/star.png" alt="star" />
+        <Star />
       </span>
     )
   }
