3108d3d1b9f7bd811ade0bb65725e1e90472bca0
armin busatlic <dzarma.busatlic@gmail.com>
2021-11-28 21:46:32 +0100

    Fixing css on ContactFormBig component

--- src/components/contactPageSection/ContactFormBig.jsx
+++ src/components/contactPageSection/ContactFormBig.jsx
@@ -15,55 +15,62 @@ const ContactFormBig = props => {
         name="form-name"
         value="learning-makeover-contact-form"
       />
-      <label className="contact-form__label">
-        {props.name}
+      <div className="text-input__full-width">
+        <label className="contact-form__label">{props.name}</label>
         <input
           type="text"
           name="name"
           id="name"
           placeholder="Type Here"
           className="contact-form__input"
+          required
         />
-      </label>
-      <label className="contact-form__label">
-        {props.subject}
+      </div>
+      <div className="text-input__full-width">
+        <label className="contact-form__label">{props.subject}</label>
         <textarea
           name="message"
           id="message"
-          rows="1"
-          className="contact-form__input"
+          placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          className="contact-form__textarea"
+          required
         />
-      </label>
-      <label className="contact-form__label">
-        {props.email}
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Type your email address"
-          className="contact-form__input"
-        />
-      </label>
-      <label className="contact-form__label">
-        {props.phone}
-        <input
-          type="phone"
-          name="phone"
-          id="phone"
-          placeholder="Type your tel number"
-          className="contact-form__input"
-        />
-      </label>
-      <label className="contact-form__label">
-        {props.web}
+      </div>
+      <div className="text-input__wrapper">
+        <div className="text-input__half-width">
+          <label className="contact-form__label">{props.email}</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Type your email address"
+            className="contact-form__input"
+            required
+          />
+        </div>
+        <div className="text-input__half-width">
+          <label className="contact-form__label">{props.phone}</label>
+          <input
+            type="phone"
+            name="phone"
+            id="phone"
+            placeholder="Type your tel number"
+            className="contact-form__input"
+            required
+          />
+        </div>
+      </div>
+      <div className="text-input__half-width">
+        <label className="contact-form__label">{props.web}</label>
         <input
           type="text"
           name="web"
           id="web"
           placeholder="Subject"
           className="contact-form__input"
+          required
         />
-      </label>
+      </div>
 
       <input className="button" type="submit" value="Submit" />
     </form>
